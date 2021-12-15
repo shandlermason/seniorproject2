c1067cfc0a742b9fc6761ed10855439627cda475
shandlermason <samason1@aggies.ncat.edu>
2021-12-15 00:21:30 -0500

    cleaned up writing and retrieving/ cleaned up comments
    Able to write and retrieve mock real-time data at 1 timestamp and improved comments

--- writemockdata.py
+++ writemockdata.py
@@ -1,5 +1,3 @@
-import json
-
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from datetime import datetime
@@ -10,29 +8,30 @@ org = "NCAT Senior Project 2"
 bucket = "senior project 2"
 
 with InfluxDBClient(url="http://localhost:8086", token=token, org=org) as client:
-    #Send mock data to database
     write_api = client.write_api(write_options=SYNCHRONOUS)
 
+    # Mock real-time fields and data at 1 timestamp
     point = Point("mem") \
         .tag("host", "host1") \
-        .field("x", 1) \
+        .field("x", 4) \
         .field("y", 2) \
+        .field("z", 3) \
         .time(datetime.utcnow(), WritePrecision.NS)
-    point2 = Point("mem2") \
-        .tag("host", "host1") \
-        .field("x", 3) \
-        .field("y", 4)\
-        .time(datetime.utcnow(), WritePrecision.NS)
-    write_api.write(bucket, org, point)
-    write_api.write(bucket, org, point2)
 
-    #Search for data within bucket and retrieve data
-    query = 'from(bucket: "senior project 2") |> range(start: -1m)'
+    # Send mock real-time data to InfluxDB
+    write_api.write(bucket, org, point)
+
+    # Search for data within bucket and retrieve data
+    # Retrieving data written between time 1 hour ago and now
+    # Filtering data to only return all x-values between the timeframe
+    query = 'from(bucket: "senior project 2") |> range(start: -1h, stop: now()) |> filter(fn:(r) => r._field == "x" )'
     tables = client.query_api().query(query, org=org)
 
-    #Print data retrieved in consol
+    # Print data retrieved in console
     for table in tables:
         for record in table.records:
             print(record)
+
+    # Have to figure out how to get just the value from the record and put value into variable
 #
 client.close()
