414880dc33aafe15e1cb7f48b2d69ee12881c2a6
shandlermason <samason1@aggies.ncat.edu>
2022-01-29 02:39:06 -0500

    twin model method - big commit
    cleaned code, added comments, create list of data for each field, twin method, finding the delta values, putting delta values in a list

--- getData.py
+++ getData.py
@@ -1,31 +1,74 @@
 from influxdb_client import InfluxDBClient
 
-# Generated API token, org and bucket
-token = "F9Mc-Unn4MGPfZIHb18W2a2FFOraMQzbqt_oQjZxlH79No3_v0kKETqnt0Cjmprzl9-VT5EtXjAr8e3Ce3w78w=="
-org = "NCAT Senior Project 2"
-bucket = "SP2"
 
-with InfluxDBClient(url="http://localhost:8086", token=token, org=org) as client:
-    query_api = client.query_api()
-    # Search for data within bucket and retrieve data
-    # Retrieving data written between time 1 hour ago and now
-    # Filtering data to only return all x-values between the timeframe
-    query = 'from(bucket: "SP2") \
-    |> range(start: -10y) \
-    |> filter(fn:(r) => r._measurement == "power")'
-    result = query_api.query(org=org, query=query)
+def retrieve_data():
+    # Generated API token, org and bucket - ask Shandler for InfluxDB username and password
+    token = "F9Mc-Unn4MGPfZIHb18W2a2FFOraMQzbqt_oQjZxlH79No3_v0kKETqnt0Cjmprzl9-VT5EtXjAr8e3Ce3w78w=="
+    org = "NCAT Senior Project 2"
+    bucket = "SP2"
 
-    # Print data retrieved in console
-    results = []
-    for table in result:
-        for record in table.records:
-            results.append((record.get_field(), record.get_value()))
+    # run InfluxDB on local host
+    with InfluxDBClient(url="http://localhost:8086", token=token, org=org) as client:
+        query_api = client.query_api()
 
-    print(results)
-    # Have to figure out how to get just the value from the record and put value into variable
-#
-#client.close()
+        # Search for data within SP2 bucket
+        query = 'from(bucket: "SP2") \
+        |> range(start: -10y) \
+        |> filter(fn:(r) => r._measurement == "power")'
+        result = query_api.query(org=org, query=query)
 
+        # Creates a list of data for each field
+        battery_soc_list = []
+        pg_body_mounted_list = []
+        pg_deployed = []
+        for table in result:
+            for record in table.records:
+                if record.get_field() == "battery_state_of_charge":
+                    battery_soc_list.append(record.get_value())
+                elif record.get_field() == "power_generated_body_mounted":
+                    pg_body_mounted_list.append(record.get_value())
+                elif record.get_field() == "power_generated_deployed":
+                    pg_deployed.append(record.get_value())
+
+        # test to check if values are appending to specific list
+        print('batter state of charge list # 1: ', battery_soc_list)
+        return battery_soc_list, pg_body_mounted_list, pg_deployed
+
+def twin_model(soc, bm, deploy):
+    twin_soc = 0
+    twin_bm = 0
+    twin_deploy = 0
+    index = 67
+    list_delta_soc = []
+
+    while True:
+        # test to check values in variables
+        print(soc[index])
+        print(twin_soc)
+        
+        delta_soc = soc[index] - twin_soc
+        if delta_soc > 0.001:
+            twin_soc = soc[index]
+            list_delta_soc.append(delta_soc)
+            # test to check delta values and if list is appending delta values
+            print('deltas one by one', list_delta_soc)
+        index += 1
+
+        if index == (len(soc) - 1):
+            False
+
+    # test to make sure delta values are calculating and appending to list
+    print('list of deltas: ', list_delta_soc)
+
+
+b_state_of_charge, pg_body_mounted, pg_deploy = retrieve_data()
+
+# tests to make sure the data is being retrieved and put into list
+print('batter soc list: ', b_state_of_charge)
+print('power generated body mounted list: ', pg_body_mounted)
+print('power generated deploy list: ', pg_deploy)
+
+twin_model(b_state_of_charge, pg_body_mounted, pg_deploy)
 
 '''
 battery_soc = (most recent data pulled from influx) 100 (figure out how to read record by record in a loop that way 
