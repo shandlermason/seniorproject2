bc18da6019c0959f4d8a5ca3a5b1ee974de23f6b
shandlermason <samason1@aggies.ncat.edu>
2022-03-29 22:03:56 -0400

    reorganized code

--- getData.py
+++ getData.py
@@ -1,5 +1,6 @@
 from influxdb_client import InfluxDBClient
 import pandas as pd
+from numpy import mean
 
 def retrieve_data(token, org, bucket, query):
     # Generated API token, org and bucket - ask Shandler for InfluxDB username and password
@@ -23,56 +24,48 @@ def retrieve_data(token, org, bucket, query):
 
 
 # organizing current, voltage values
-def organize_data_C_V(r_data_c_v):
+def separating_c_and_v(data_c_v):
     list_of_currents = []
     list_of_voltage = []
     count = 0
-    while count < len(r_data_c_v):
-        if "_C" in r_data_c_v[count][0]:
-            list_of_currents.append(r_data_c_v[count])
+    while count < len(data_c_v):
+        if "_C" in data_c_v[count][0]:
+            list_of_currents.append(data_c_v[count])
         else:
-            list_of_voltage.append(r_data_c_v[count])
+            list_of_voltage.append(data_c_v[count])
         count += 1
 
-    # empty dictionaries
-    freq_c = {}
-    freq_v = {}
-    # iterates through real telemetry current and voltage
-    for element in list_of_currents:
-        '''Sets the default of the dictionary making the time the key value and the field/value the value. 
-        .setdefault() allows for multiple values to have the same key. The purpose of the dictionary is to sort 
-        the field/value by there times. Sorting by the time will allows us to sum the current values 
-        of all 8 solar panels on 1 satellite.'''
-        freq_c.setdefault((element[2].strftime("%Y-%m-%d %H:%M:%S")), []).append(element[1])
-    for element in list_of_voltage:
-        '''Sets the default of the dictionary making the time the key value and the field/value the value. 
-        .setdefault() allows for multiple values to have the same key. The purpose of the dictionary is to sort 
-        the field/value by there times. Sorting by the time will allows us to sum the current values 
-        of all 8 solar panels on 1 satellite.'''
-        freq_v.setdefault((element[2].strftime("%Y-%m-%d %H:%M:%S")), []).append(element[1])
-
-    # sum the current values of solar panels 1-8 with the same keys (the same times)
-    temp = list(freq_c)
-    test_key = temp[0]
-    for key, values in freq_c.items():
-        # updates the dictionary to have the current of the satellite (all 8 solar panels) at a specific time
-        freq_c[key] = (sum(values)/8) # sum all values and divide by 8 to get the average current for satellite at each time
-        # goes to the next key in the dictionary
-        temp[temp.index(test_key) + 1]
-
-    # sum the voltage values of solar panels 1-8 with the same keys (the same times)
-    temp = list(freq_v)
-    test_key = temp[0]
-    for key, values in freq_v.items():
-        # updates the dictionary to have the voltage of the satellite (all 8 solar panels) at a specific time
-        freq_v[key] = (sum(values)/8) # sum all values and divide by 8 to get the average voltage for satellite at each time
-        # goes to the next key in the dictionary
-        temp[temp.index(test_key) + 1]
-    return freq_c, freq_v
+    return list_of_currents, list_of_voltage
 
 
 def organize_power(power_data):
     return 0
+
+
+# f
+def organize_data(data_list):
+
+    # empty dictionaries
+    freq = {}
+    # iterates through real telemetry current and voltage
+    for element in data_list:
+        '''Sets the default of the dictionary making the time the key value and the field/value the value. 
+        .setdefault() allows for multiple values to have the same key. The purpose of the dictionary is to sort 
+        the field/value by there times. Sorting by the time will allows us to sum the current/voltage/power values 
+        of all 8 solar panels on 1 satellite.'''
+        freq.setdefault((element[2].strftime("%Y-%m-%d %H:%M:%S")), []).append(element[1])
+
+    # sum the current values of solar panels 1-8 with the same keys (the same times)
+    temp = list(freq)
+    test_key = temp[0]
+    for key, values in freq.items():
+        # updates the dictionary to have the current of the satellite (all 8 solar panels) at a specific time
+        freq[key] = mean(values) # sum all values and divide by 8 to get the average current for satellite at each time
+        # goes to the next key in the dictionary
+        temp[temp.index(test_key) + 1]
+
+    return freq
+
 
 # creates timestamp values for every 10 seconds and fills in with estimated values
 def analyze_data(vals):
@@ -101,9 +94,15 @@ data_set_2 = retrieve_data("F9Mc-Unn4MGPfZIHb18W2a2FFOraMQzbqt_oQjZxlH79No3_v0kK
         query = 'from(bucket: "Simulated_Telemetry_p") \
         |> range(start: -10y) \
         |> filter(fn:(r) => r._measurement == "power")')
-dict_c, dict_v = organize_data_C_V(data_set_1)
-dict_p = organize_power(data_set_2)
-analyze_data(dict_c)
-analyze_data(dict_v)
-analyze_data(dict_p)
+c_list, v_list = separating_c_and_v(data_set_1)
+p_list = organize_power(data_set_2)
+
+dict_c = organize_data(c_list)
+dict_v = organize_data(v_list)
+
+dict_p = organize_data(p_list)
+
+inter_1 = analyze_data(dict_c)
+inter_2 = analyze_data(dict_v)
+inter_3 = analyze_data(dict_p)
 
